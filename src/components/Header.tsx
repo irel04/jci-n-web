04d6ed0a7cffb2432aa11f5fcb40be1c43d37607
Irel Bacolod <116436224+irel04@users.noreply.github.com>
2025-03-20 17:04:51 +0800

    modify docs nav to FAQS

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 // import logo from "@public/assets/logo.png"
 import githubLogo from "@src/assets/icons/github-logo.svg"
-import docs from "@src/assets/icons/docs.svg"
+import faqs from "@src/assets/icons/faqs.svg"
 
 function Header() {
 	return (
@@ -8,14 +8,13 @@ function Header() {
 			<div className="flex-1">
 				<button className="bg-neutral-500/50 p-4  w-36 text-center rounded-md text-white">Logo Icon</button>
 			</div>
-
-			<button className="flex-none flex items-center gap-2">
-				<img src={docs} alt="" className="w-7 aspect-auto" title="Checkout Repo" />
-				<p className="hidden md:block">Docs</p>
-			</button>
 			<button className="flex-none flex items-center gap-2">
 				<img src={githubLogo} alt="" className="w-7 aspect-auto" title="Checkout Repo" />
 				<p className="hidden md:block">Checkout Our Repo</p>
+			</button>
+			<button className="flex-none flex items-center gap-2">
+				<img src={faqs} alt="" className="w-8 aspect-auto" title="Frequent Ask Questions" />
+				<p className="hidden md:block">FAQS</p>
 			</button>
 
 		</header>
