dcb407d09556c5181923a35d8627b2a4666bdbe9
Irel Bacolod <116436224+irel04@users.noreply.github.com>
2025-03-20 13:08:41 +0800

    build: initial setup

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+
+
+const Home = () => {
+	return (
+		<div className="text-">Home</div>
+	)
+}
+
+export default Home
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+
+
+// Convert `import.meta.url` to a directory path
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  resolve: {
+    alias: {
+      '@src': path.resolve(__dirname, 'src')
+    }
+  }
+})
