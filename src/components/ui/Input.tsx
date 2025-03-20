80f0d88a4b13b62928d88fbf8edc266229c8f8db
Irel Bacolod <116436224+irel04@users.noreply.github.com>
2025-03-20 19:03:12 +0800

    fix: modify input component offsets

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -7,7 +7,7 @@ type InputProps = InputHTMLAttributes<HTMLInputElement> & {
 
 const Input = ({ icon, label, ...props }: InputProps) => {
 	return (
-		<div className="flex flex-col max-w-48 text-neutral-400 gap-1 text-sm md:text-base">
+		<div className="flex flex-col max-w-60 text-neutral-400 gap-1 text-sm md:text-base ">
 			<label htmlFor={label} className="text-neutral-600">{label}</label>
 			<div className="border-neutral-400 border-[1px] rounded-md p-2">
 				<input id={label.toLowerCase()} {...props} className="focus:outline-none w-full" />
