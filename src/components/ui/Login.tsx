ab49b55c806c8a36404e4d72719717d68789dc23
Irel Bacolod <116436224+irel04@users.noreply.github.com>
2025-03-21 22:47:24 +0800

    style: modify login to overlaying modal

--- src/components/ui/Login.tsx
+++ src/components/ui/Login.tsx
@@ -1,15 +1,24 @@
 import Input from "@src/components/ui/Input"
+import pupLogo from "@src/assets/pup-logo.png"
+import { useEffect } from "react"
 
 const Login = () => {
-	return (
-		<div className="w-full max-w-[320px] md:max-w-[500px] shadow-lg mx-auto p-4 flex flex-col justify-center items-center text-brand-700 gap-5 rounded-md bg-neutral-200/50 h-full max-h-[300px]">
-			<p className="text-lg font-bold md:text-xl">Login</p>
 
-			<form className="flex flex-col gap-4">
-				<Input placeholder="Email"/>
-				<Input placeholder="Password" type="password"/>
-				<button className="px-4 py-2 bg-brand-300 rounded-md text-app-white max-w-max mx-auto text-sm md:text-base" type="submit">Continue</button>
-			</form>
+	useEffect(() => {
+
+	}, [])
+
+	return (
+		<div className="fixed inset-0 z-20 bg-black/80 p-4 md:p-8">
+			<div className="w-full max-w-[320px] md:max-w-[500px] shadow-lg flex flex-col items-center text-brand-700 gap-5 rounded-md p-8 mx-auto bg-app-white space-y-2 h-full ">
+				<img src={pupLogo} alt="" className="" />
+				<p className="text-xl font-bold">Login</p>
+				<form className="flex-1 justify-center items-center flex flex-col gap-4">
+					<Input label="Email" />
+					<Input type="password" label="Password" />
+					<button className="px-4 py-2 bg-brand-300 rounded-md text-app-white max-w-max mx-auto text-sm md:text-base" type="submit">Continue</button>
+				</form>
+			</div>
 		</div>
 	)
 }
