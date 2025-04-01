cbcec74aa1a0b653415742fe7dba7e0bc5714e57
Irel Bacolod <116436224+irel04@users.noreply.github.com>
2025-04-01 12:33:56 +0800

    fix: update labels htmlfor

--- src/components/EditUser.tsx
+++ src/components/EditUser.tsx
@@ -70,11 +70,11 @@ const EditUser = ({ isOpen, onClose, userData }: Props) => {
 						<Input className={`col-span-3 ${errors.last_name ? "border-red-500 border-[1.2px]" : ""}`} {...register("last_name")} />
 					</div>
 					<div className="grid grid-cols-4 items-center gap-4">
-						<Label htmlFor="username" >First Name</Label>
+						<Label htmlFor="first_name" >First Name</Label>
 						<Input className={`col-span-3 ${errors.first_name ? "border-red-500 border-[1.2px]" : ""}`}  {...register("first_name")} />
 					</div>
 					<div className="grid grid-cols-4 items-center gap-4">
-						<Label htmlFor="username" >Birthday</Label>
+						<Label htmlFor="birthdate" >Birthday</Label>
 						<Controller control={control} name="birthdate" render={({ field: { value, onChange } }) => (
 							<div className="col-span-3">
 								<InputCalendar variant="shadcn" defaultValue={value} onChange={onChange} error={errors.birthdate}/>
@@ -82,23 +82,23 @@ const EditUser = ({ isOpen, onClose, userData }: Props) => {
 						)}/>
 					</div>
 					<div className="grid grid-cols-4 items-center gap-4">
-						<Label htmlFor="username" >Email</Label>
+						<Label htmlFor="email_address" >Email</Label>
 						<Input className={`col-span-3 ${errors.email_address ? "border-red-500 border-[1.2px]" : ""}`} {...register("email_address")} />
 					</div>
 					<div className="grid grid-cols-4 items-center gap-4">
-						<Label htmlFor="username" >Address</Label>
+						<Label htmlFor="address" >Address</Label>
 						<Input className={`col-span-3 ${errors.address ? "border-red-500 border-[1.2px]" : ""}`}  {...register("address")} />
 					</div>
 					<div className="grid grid-cols-4 items-center gap-4">
-						<Label htmlFor="username">Mobile No.</Label>
+						<Label htmlFor="phone_number">Mobile No.</Label>
 						<Input className={`col-span-3 ${errors.phone_number ? "border-red-500 border-[1.2px]" : ""}`}   {...register("phone_number")} />
 					</div>
 					<div className="grid grid-cols-4 items-center gap-4">
-						<Label htmlFor="username" >RFID</Label>
+						<Label htmlFor="RFID" >RFID</Label>
 						<Input className={`col-span-3 ${errors.RFID ? "border-red-500 border-[1.2px]" : ""}`} {...register("RFID")} />
 					</div>
 					<div className="grid grid-cols-4 items-center gap-4">
-						<Label htmlFor="username" >Status</Label>
+						<Label htmlFor="status" >Status</Label>
 						<Controller control={control} name="status" render={({ field: { onChange, value } }) => (
 							<DropdownMenu>
 								<DropdownMenuTrigger className="col-span-3">
